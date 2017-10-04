f5a52f626d4bc2f02669a13ed8714e6bc11ab7e8
Antoine Rahier <antoine.rahier@gmail.com>
2017-10-04 14:10:15 +0200

    Add speedtest alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,2 +1,3 @@
 alias fu='sudo $(fc -ln -1)'
 alias dfu='git -C ~/.dotfiles pull -q && ~/.dotfiles/.install -Q && exec zsh'
+alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -'
