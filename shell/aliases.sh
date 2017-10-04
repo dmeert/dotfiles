c0f20312b4d62c191454a32d5fee117866590553
Antoine Rahier <antoine.rahier@gmail.com>
2017-10-04 17:49:46 +0200

    Update aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,3 +1,4 @@
-alias fu='sudo $(fc -ln -1)'
 alias dfu='git -C ~/.dotfiles pull -q && ~/.dotfiles/.install -Q && exec zsh'
+alias fu='sudo $(fc -ln -1)'
 alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -'
+alias type='type -f'
