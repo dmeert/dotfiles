47285bd88663d722f8d4d5b72c575e476a3c6328
Antoine Rahier <Twanislas@users.noreply.github.com>
2018-10-20 01:26:38 +0200

    Update install-dotfiles.sh
    Git submodules does not follow core.worktree setting :/

--- .bin/install-dotfiles.sh
+++ .bin/install-dotfiles.sh
@@ -9,15 +9,15 @@ fi
 
 # Clone repo and set it up
 git clone --bare git@github.com:Twanislas/dotfiles.git ${HOME}/${DOTFILES}
-git -C ${HOME}/${DOTFILES} config --local core.bare no
-git -C ${HOME}/${DOTFILES} config --local core.worktree ${HOME}
-git -C ${HOME}/${DOTFILES} config --local status.showUntrackedFiles no
+git --git-dir=$HOME/$DOTFILES --work-tree=$HOME config --local core.bare no
+git --git-dir=$HOME/$DOTFILES --work-tree=$HOME config --local core.worktree ${HOME}
+git --git-dir=$HOME/$DOTFILES --work-tree=$HOME config --local status.showUntrackedFiles no
 echo '*' >> ${HOME}/${DOTFILES}/info/exclude
 echo '.*' >> ${HOME}/${DOTFILES}/info/exclude
 
 # Pull files
-git -C ${HOME}/${DOTFILES} checkout -f
-git -C ${HOME}/${DOTFILES} submodule update --recursive --init --depth 1
+git --git-dir=$HOME/$DOTFILES --work-tree=$HOME checkout -f
+git --git-dir=$HOME/$DOTFILES --work-tree=$HOME submodule update --recursive --init
 
 # Spawn a new shell
 exec ${SHELL}
