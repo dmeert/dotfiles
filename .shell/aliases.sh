b4382fe401e69657b6c251b5301fda9fa3857a93
Antoine Rahier <antoine.rahier@gmail.com>
2018-10-20 18:42:57 +0200

    Add Docker aliases, prettify

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -1,5 +1,12 @@
+# Dotfiles
 alias dotfiles='git --git-dir=$HOME/$DOTFILES --work-tree=$HOME'
 alias dfu='dotfiles pull -q && dotfiles submodule update -q --init --recursive && exec $(which zsh)'
+
+# Docker
+alias dc='docker-compose'
+alias dk='docker'
+
+# Misc
 alias fu='sudo $(fc -ln -1)'
 alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -'
 alias type='type -f'
