800ffdd253a38dc34357c6257deed04ed48081ad
Antoine Rahier <antoine.rahier@gmail.com>
2018-10-20 11:33:38 +0200

    Update dotfiles update alias

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -1,5 +1,5 @@
 alias dotfiles='git --git-dir=$HOME/$DOTFILES --work-tree=$HOME'
-alias dfu='git -C ~/.dotfiles pull -q && ~/.dotfiles/.install -Q && exec zsh'
+alias dfu='dotfiles pull -q && dotfiles submodule update -q --init --recursive && exec $(which zsh)'
 alias fu='sudo $(fc -ln -1)'
 alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -'
 alias type='type -f'
