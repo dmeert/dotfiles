5b0bb5b287e3488383c46e54d8b104fe350f4704
Antoine Rahier <antoine.rahier@gmail.com>
2019-10-11 21:20:45 +0200

    Prevent Linux OS runtimes on WSL

--- .shell/os_linux.sh
+++ .shell/os_linux.sh
@@ -2,7 +2,7 @@
 # Linux specifics
 #
 
-if [[ "$OSTYPE" =~ ^linux ]]; then
+if [[ "$OSTYPE" =~ ^linux ]] && [[ -f /proc/version ]] && grep -v --quiet Microsoft /proc/version ; then
 
   # Link VSCode config
   ln -sf "$HOME/.vscode/settings.json" "$HOME/.config/Code/User/settings.json"
