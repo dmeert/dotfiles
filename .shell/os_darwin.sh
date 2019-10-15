551bf66104fc392c6ed8608d02683e60868dda19
Antoine Rahier <antoine.rahier@gmail.com>
2019-10-15 15:05:23 +0200

    Create dir for VSCode config

--- .shell/os_darwin.sh
+++ .shell/os_darwin.sh
@@ -5,6 +5,7 @@
 if [[ "$OSTYPE" =~ ^darwin ]]; then
 
   # Link VSCode config
+  mkdir -p "$HOME/Library/Application Support/Code/User"
   ln -sf "$HOME/.vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
 
 fi
--- .shell/os_linux.sh
+++ .shell/os_linux.sh
@@ -5,6 +5,7 @@
 if [[ "$OSTYPE" =~ ^linux ]] && [[ -f /proc/version ]] && grep -v --quiet Microsoft /proc/version ; then
 
   # Link VSCode config
+  mkdir -p "$HOME/.config/Code/User"
   ln -sf "$HOME/.vscode/settings.json" "$HOME/.config/Code/User/settings.json"
 
 fi
