5d59277f6e21734a266d9e11b5909a8973046326
Antoine Rahier <524706+Twanislas@users.noreply.github.com>
2020-10-22 14:05:58 +0200

    Update install script

--- .bin/install-dotfiles.sh
+++ .bin/install-dotfiles.sh
@@ -1,28 +1,37 @@
+#!/bin/bash
 #
 # Install dotfiles on new machine
 #
 export DOTFILES=${DOTFILES:-".dotfiles"}
-if [ -e ${HOME}/${DOTFILES} ]; then
+if [ -e "${HOME}/${DOTFILES}" ]; then
     echo "${HOME}/${DOTFILES} already exists !"
     exit 1
 fi
 
 # Clone repo and set it up
-git clone --bare git@github.com:Twanislas/dotfiles.git ${HOME}/${DOTFILES}
-git --git-dir=${HOME}/${DOTFILES} --work-tree=${HOME} config --local core.bare no
-git --git-dir=${HOME}/${DOTFILES} --work-tree=${HOME} config --local core.worktree ${HOME}
-git --git-dir=${HOME}/${DOTFILES} --work-tree=${HOME} config --local status.showUntrackedFiles no
-git --git-dir=${HOME}/${DOTFILES} --work-tree=${HOME} config --local core.sparsecheckout true
-echo '*' >> ${HOME}/${DOTFILES}/info/exclude
-echo '.*' >> ${HOME}/${DOTFILES}/info/exclude
-echo '/*' >> ${HOME}/${DOTFILES}/info/sparse-checkout
-echo '!README.md' >> ${HOME}/${DOTFILES}/info/sparse-checkout
-echo '!LICENCE' >> ${HOME}/${DOTFILES}/info/sparse-checkout
-echo '!.github' >> ${HOME}/${DOTFILES}/info/sparse-checkout
+git clone --bare git@github.com:Twanislas/dotfiles.git "${HOME}/${DOTFILES}"
+git --git-dir="${HOME}/${DOTFILES}" --work-tree="${HOME}" config --local core.bare no
+git --git-dir="${HOME}/${DOTFILES}" --work-tree="${HOME}" config --local core.worktree "${HOME}"
+git --git-dir="${HOME}/${DOTFILES}" --work-tree="${HOME}" config --local status.showUntrackedFiles no
+git --git-dir="${HOME}/${DOTFILES}" --work-tree="${HOME}" config --local core.sparsecheckout true
+{
+    echo '*'
+    echo '.*'
+    echo '/*'
+} >> "${HOME}/${DOTFILES}"/info/exclude
+{
+    echo '/*'
+    echo '!README.md'
+    echo '!LICENCE'
+    echo '!.github'
+} >> "${HOME}/${DOTFILES}"/info/sparse-checkout
 
 # Pull files
-git --git-dir=${HOME}/${DOTFILES} --work-tree=${HOME} checkout -f
-git --git-dir=${HOME}/${DOTFILES} --work-tree=${HOME} submodule update --recursive --init
+git --git-dir="${HOME}/${DOTFILES}" --work-tree="${HOME}" checkout -f
+git --git-dir="${HOME}/${DOTFILES}" --work-tree="${HOME}" submodule update --recursive --init
+
+# Change default shell to Zsh
+chsh -s "$(which zsh)"
 
 # Spawn a new shell
-exec $(which zsh)
+exec "$(which zsh)"
