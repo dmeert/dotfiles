e34d610bedc996b6d49f9fdeba3cd7d3102b564c
Antoine Rahier <524706+Twanislas@users.noreply.github.com>
2020-10-24 21:11:39 +0200

    Remove VSCode config (part 2)

--- .shell/os_darwin.zsh
+++ .shell/os_darwin.zsh
@@ -4,8 +4,6 @@
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
 
-  # Link VSCode config
-  mkdir -p "$HOME/Library/Application Support/Code/User"
-  ln -sf "$HOME/.vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+  # MacOS stuff here
 
 fi
--- .shell/os_linux.zsh
+++ .shell/os_linux.zsh
@@ -4,8 +4,6 @@
 
 if [[ "$OSTYPE" =~ ^linux ]] && [[ -f /proc/version ]] && grep -v --quiet Microsoft /proc/version ; then
 
-  # Link VSCode config
-  mkdir -p "$HOME/.config/Code/User"
-  ln -sf "$HOME/.vscode/settings.json" "$HOME/.config/Code/User/settings.json"
+  # Linux stuff here
 
 fi
