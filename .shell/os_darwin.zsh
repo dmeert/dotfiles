e2603a9e4f9dbcb899e32950c9fb85c801753362
Twanislas <524706+Twanislas@users.noreply.github.com>
2024-01-30 17:34:35 +0100

    Add VSCode in path for Darwin

--- .shell/os_darwin.zsh
+++ .shell/os_darwin.zsh
@@ -6,4 +6,7 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
 
   # MacOS stuff here
 
+  # Add Visual Studio Code (code)
+  export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
 fi
